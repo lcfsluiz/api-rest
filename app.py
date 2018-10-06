a7b8ed980d880864c086b6d120d767008dcc2571
daniel prata <daniel.prata@4linux.com.br>
2018-10-06 17:54:48 -0300

    aula1 python for sysadmin

--- app.py
+++ app.py
@@ -1,7 +1,69 @@
 #!/usr/bin/python3
-
+import json
 import requests
+from pymongo import MongoClient
+from flask import (Flask, jsonify, make_response,
+                    redirect, Response, request)
 
-requisicao = requests.get("https://viacep.com.br/ws/08270630/json/")
+try:
+    con = MongoClient()
+    db = con['projeto']
+except Exception as e:
+    print('Erro:{}'.format(e))
+    exit()
 
-print(requisicao.json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    mensagem = {'mensagem': 'minha api'}
+    return jsonify(mensagem)
+
+
+@app.route('/usuarios', methods=['GET'])
+def usuarios():
+    return jsonify([x for x in db.usuarios.find()])
+
+
+@app.route('/usuario/<int:id>', methods=['GET'])
+def usuario(id):
+    busca = db.usuarios.find_one({"_id": id})
+    if busca:
+        return jsonify(busca)
+    else:
+        busca = {"mensagem": "Usuario não encontrado"}
+        return Response(json.dumps(busca), 404, content_type="application/json")
+
+    # return jsonify(busca) if busca else Response(json.dumps({"mensagem":"Usuario não encontrado"}), 404, content_type="application/json")
+
+@app.route('/usuario', methods=['POST'])
+def registrar_usuario():
+    try:
+        data = request.get_json()
+        if data["_id"] and isinstance(data["_id"], int):
+            db.usuarios.insert(request.get_json())
+            return jsonify({"mensagem":"cadastrado com sucesso!"})  
+        else:
+            return jsonify({"mensagem":"id invalido"})
+    except Exception:
+        return jsonify({"mensagem": "usuario ja cadastrado"})
+
+
+    # print(data["_id"])
+
+
+@app.route('/cep/<busca>')
+def buscar_cep(busca):
+    requisicao = requests.get("https://viacep.com.br/ws/{}/json/".format(busca))
+    return jsonify(requisicao.json())
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',  port=5000,  debug=True)
+
+
+# import requests
+# requisicao = requests.get("https://viacep.com.br/ws/08270630/json/")
+# print(requisicao.json()
